54a41c442f21efd53b176491e6d214913cc1bc6a
Markus <Markus@192.168.0.196>
2010-09-07 20:26:13 +0200

    adding pds.web.ui.app.whisper

---
+++ pds.web.ui.app.whisper/src/main/java/pds/web/ui/app/whisper/WhisperPdsWebApp.java
@@ -0,0 +1,31 @@
+package pds.web.ui.app.whisper;
+
+import nextapp.echo.app.ResourceImageReference;
+
+import org.eclipse.higgins.xdi4j.xri3.impl.XRI3Segment;
+
+import pds.web.ui.MainContentPane;
+import pds.web.ui.MessageDialog;
+import pds.web.ui.app.PdsWebApp;
+import pds.web.xdi.XdiContext;
+
+public class WhisperPdsWebApp implements PdsWebApp {
+
+	@Override
+	public String getName() {
+
+		return "Whisper";
+	}
+
+	@Override
+	public ResourceImageReference getResourceImageReference() {
+
+		return new ResourceImageReference("/pds/web/ui/app/whisper/app.png");
+	}
+
+	@Override
+	public void onActionPerformed(MainContentPane mainContentPane, XdiContext context, XRI3Segment subjectXri) {
+
+		MessageDialog.notImplemented();
+	}
+}
