b37e17a11ff89ec553bfd76272998943ea24871b
Markus Sabadello <markus@mac.local>
2010-09-10 02:52:46 +0200

    removing pds.discovery and pds.web.xdi

--- pds.web.xdi/src/main/java/pds/web/xdi/events/XdiTransactionListener.java
+++
@@ -1,7 +0,0 @@
-package pds.web.xdi.events;
-
-public interface XdiTransactionListener {
-
-	public void onXdiTransactionSuccess(XdiTransactionSuccessEvent xdiTransactionSuccessEvent);
-	public void onXdiTransactionFailure(XdiTransactionFailureEvent xdiTransactionFailureEvent);
-}
