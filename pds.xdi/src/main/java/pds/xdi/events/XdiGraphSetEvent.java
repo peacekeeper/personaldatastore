0ec82dbb02f04858b645644e42a963ea97e9a989
Markus <Markus@192.168.0.196>
2010-09-12 04:13:37 +0200

    fix

---
+++ pds.xdi/src/main/java/pds/xdi/events/XdiGraphSetEvent.java
@@ -0,0 +1,13 @@
+package pds.xdi.events;
+
+
+
+public class XdiGraphSetEvent extends XdiGraphEvent {
+
+	private static final long serialVersionUID = 4168011281824943892L;
+
+	public XdiGraphSetEvent(Object source) {
+
+		super(source);
+	}
+}
